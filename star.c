55c8e1e683da0df848ec35bb4e9308f94eee7e0c
xtrship <rainwol@outlook.com>
2025-01-29 16:14:08 +1100

    added starter files

---
+++ star.c
@@ -0,0 +1,13 @@
+
+// main c file
+#include "star.h"
+
+
+int main(void) {
+	// print test
+	printf("Hello World");
+
+	return EXIT_SUCCESS;
+}
+
+
---
+++ star.h
@@ -0,0 +1,8 @@
+#ifndef STAR_H
+# define STAR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
+
