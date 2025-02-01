050cf08283f2060e66b93d946bd8d6a147a649ac
xtrship <rainwol@outlook.com>
2025-02-01 14:35:07 +1100

    ansi colour codes added to header

--- star.c
+++ star.c
@@ -2,8 +2,42 @@
 #include "star.h"
 
 
+char 	*star_read_line(void
+){
+
+	char 	*buf;
+	size_t	bufsize;
+
+	buf = NULL; // getline will take care of this 
+	
+	if (getline(&buf, &bufsize, stdin) == -1)
+	{
+		if (feof(stdin))
+		    printf("[EOF]");
+		else 
+		    printf("GETLINE FAILED!");
+	}
+
+}
+
+
 int main(int ac, char **av)
 {
+
+	char 	*line;
+
+
+	while (0xCE77)
+	{
+	//1) GET LINE
+	line = star_read_line(); 
+	
+	//2) GET TOKENS 
+	
+	//3) EXEC
+	
+	}
+
         (void)ac;
         int status;
 
--- star.h
+++ star.h
@@ -1,4 +1,4 @@
-// star.h
+`// star.h
 #ifndef STAR_H
 #define STAR_H
 
@@ -7,6 +7,20 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
+
+/*
+** ANSI Color codes for terminal output formatting:
+** Y    - Yellow
+** G    - Green
+** C    - Cyan
+** RED  - Red
+** RST  - Reset to default color
+*/
+#define Y		"\033[1;33m"
+#define G		"\033[1;32m"
+#define C 		"\033[1;36m"
+#define RED		"\033[1;31m"
+#define RST 		"\033[0m"
 
 #endif
 
